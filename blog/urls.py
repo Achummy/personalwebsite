20d34e84a2445a02ddd9d882ff7c091b1211cb2c
Alex Chum <AlexChum@Alexs-MacBook-Air.local>
2017-07-26 18:55:20 -0400

    Setting up html and css files

---
+++ blog/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    url(r'^about$', views.about, name='about'),
+    url(r'^blog$', views.blog, name='blog'),
+    url(r'^blog/design_post$', views.design_post, name='design_post'),
+    url(r'^blog/lifestyle_post$', views.lifestyle_post, name='lifestyle_post'),
+]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request, 'blog/index.html', {})
+
+def about(request):
+    return render(request, 'blog/about.html', {})
+
+def blog(request):
+    return render(request, 'blog/blog.html', {})
+
+def design_post(request):
+    return render(request, 'blog/design_post.html', {})
+
+def lifestyle_post(request):
+    return render(request, 'blog/lifestyle_post.html', {})
