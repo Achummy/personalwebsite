fffd8fbadb76f7a63ceb52776b5bc074353b61df
Alex Chum <AlexChum@Alexs-MacBook-Air.local>
2017-07-28 19:13:48 -0400

    attempt to fix urls

--- blog/urls.py
+++ blog/urls.py
@@ -5,8 +5,8 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^about/$', views.about, name='about'),
     url(r'^portfolio/$', views.portfolio, name='portfolio'),
-    url(r'^contact/$', views.contact, name='contact'),
     url(r'^blog/$', views.blog, name='blog'),
     url(r'^blog/design_post/$', views.design_post, name='design_post'),
     url(r'^blog/lifestyle_post/$', views.lifestyle_post, name='lifestyle_post'),
+    url(r'^contact/$', views.contact, name='contact'),
 ]
