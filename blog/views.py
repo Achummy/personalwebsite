ef039a54e72bba037b29c0c814e33ed510cdafd8
Alex Chum <AlexChum@Alexs-MacBook-Air.local>
2017-07-30 23:29:14 -0400

    Added Blog's images

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 # Create your views here.
 def index(request):
@@ -11,13 +13,25 @@ def portfolio(request):
     return render(request, 'blog/portfolio.html', {})
 
 def blog(request):
-    return render(request, 'blog/blog.html', {})
+	return render(request, 'blog/blog.html', {})
 
 def design_post(request):
-    return render(request, 'blog/design_post.html', {})
+	posts = Post.objects.filter(published_date__lte=timezone.now()).filter(category="Design").order_by('published_date')
+	return render(request, 'blog/design_post.html', {'posts':posts})
 
 def lifestyle_post(request):
-    return render(request, 'blog/lifestyle_post.html', {})
+	posts = Post.objects.filter(published_date__lte=timezone.now()).filter(category="Lifestyle").order_by('published_date')
+	return render(request, 'blog/lifestyle_post.html', {'posts':posts})
 
 def contact(request):
     return render(request, 'blog/contact.html', {})
+
+def post(request, post_title):
+	context_dict = {}
+	try:
+		posts = Post.objects.get(slug=post_title)
+		context_dict['post_title'] = posts.title
+		context_dict['posts'] = posts
+	except Post.DoesNotExist:
+		pass
+	return render(request, 'blog/post.html', context_dict)
